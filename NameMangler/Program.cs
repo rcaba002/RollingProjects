893cf9e3ee3f7930ef385f53c8f8562dbdd7a9e7
Andrew Benz <andorbal@gmail.com>
2015-05-08 19:36:19 -0500

    Add dummy get names method

--- NameMangler/Program.cs
+++ NameMangler/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,15 @@ namespace NameMangler
             string[] names = GetNames();
             string[] sortedNames = SortNames(names);
             WriteNamesToFile("./sortedNames.txt", sortedNames);
+        }
+
+        private static string[] GetNames()
+        {
+            return new [] {
+                "John Doe",
+                "Jane Doe",
+                "Superman"
+            };
         }
     }
 }
