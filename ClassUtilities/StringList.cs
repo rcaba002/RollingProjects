e8a6b81f13e2952935caad89ec8e476ec29cf008
Andrew Benz <andorbal@gmail.com>
2015-05-11 18:58:24 -0500

    Add initial tests for StringList

---
+++ ClassUtilities/StringList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassUtilities
+{
+    public class StringList
+    {
+        private string[] passedInArray;
+
+        public StringList()
+        {
+
+        }
+
+        public StringList(string[] passedInArray)
+        {
+            this.passedInArray = passedInArray;
+            Length = passedInArray.Length;
+        }
+
+        public int Length { get; set; }
+
+        public string Get(int p)
+        {
+            return passedInArray[p];
+        }
+    }
+}
---
+++ ClassUtilitiesTests/StringListTests.cs
@@ -0,0 +1,37 @@
+﻿using ClassUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ClassUtilitiesTests
+{
+    public class StringListTests
+    {
+        [Fact]
+        public void Constructor_HasCountOfZero_WhenThereAreNoParameters()
+        {
+            StringList list = new StringList();
+            Assert.Equal(0, list.Length);
+        }
+
+        [Fact]
+        public void Constructor_HasCountOfThree_WhenExistingArrayHasThreeItems()
+        {
+            string[] passedInArray = new string[3];
+            StringList list = new StringList(passedInArray);
+            Assert.Equal(3, list.Length);
+        }
+
+        [Fact]
+        public void Constructor_HasSameItems_AsPassedInArray()
+        {
+            var passedInArray = new[] { "Foo", "Bar" };
+            StringList list = new StringList(passedInArray);
+            Assert.Equal("Foo", list.Get(0));
+            Assert.Equal("Bar", list.Get(1));
+        }
+    }
+}
