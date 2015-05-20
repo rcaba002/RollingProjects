f474e14fbf7ee571063f592907d83500f54e1592
Andrew Benz <andorbal@gmail.com>
2015-05-20 18:39:45 -0500

    Add gravatar image

---
+++ SimpleContactManager/GravatarEmailConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SimpleContactManager
+{
+    public class GravatarEmailConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string emailAddress = value as string;
+            if (emailAddress == null)
+            {
+                return null;
+            }
+
+            byte[] emailHash = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(emailAddress));
+            string emailHashString = emailHash.Select(x => x.ToString("x2")).Aggregate((x, y) => x + y);
+
+            return string.Format("http://www.gravatar.com/avatar/{0}?d=identicon&s=400", emailHashString);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SimpleContactManager/MainWindow.xaml.cs
+++ SimpleContactManager/MainWindow.xaml.cs
@@ -29,6 +29,7 @@ namespace SimpleContactManager
             contacts = new List<Contact> {
                 new Contact("John Doe", "jdoe@example.com", "314-555-1234", "123 Main"),
                 new Contact("Jane Doe", "jadoe@example.com", "314-555-3122", "345 Main"),
+                new Contact("Andrew Benz", "andorbal@gmail.com", "", ""),
                 new Contact("Superman", "superman@example.com", "314-555-1235", "Fortress of Solitude")
             };
 
