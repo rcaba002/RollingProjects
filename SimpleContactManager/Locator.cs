e9644208ed4e5da55129f3d36c2a8889a49aca1c
Andrew Benz <andorbal@gmail.com>
2015-05-20 19:07:51 -0500

    Extract view model

---
+++ SimpleContactManager/Locator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleContactManager
+{
+    public class Locator
+    {
+        public static MainViewModel Main
+        {
+            get
+            {
+                return new MainViewModel();
+            }
+        }
+    }
+}
---
+++ SimpleContactManager/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleContactManager
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            Contacts = new ObservableCollection<Contact>(new [] {
+                new Contact("John Doe", "jdoe@example.com", "314-555-1234", "123 Main"),
+                new Contact("Jane Doe", "jadoe@example.com", "314-555-3122", "345 Main"),
+                new Contact("Andrew Benz", "andorbal@gmail.com", "", ""),
+                new Contact("Superman", "superman@example.com", "314-555-1235", "Fortress of Solitude")
+            });
+        }
+
+        public ObservableCollection<Contact> Contacts { get; set; }
+    }
+}
