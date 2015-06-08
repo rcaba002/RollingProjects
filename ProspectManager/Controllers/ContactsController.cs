cf0d04a56d4bfe00b840cf6c9946c7b7a09f1373
Rob C <robcabardo@gmail.com>
2015-06-08 16:27:42 -0500

    Update Contact to create, view, and delete

--- ProspectManager/Controllers/ContactsController.cs
+++ ProspectManager/Controllers/ContactsController.cs
@@ -4,8 +4,10 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using ProspectManager.Models.TodoItems;
 
 namespace ProspectManager.Controllers
 {
@@ -42,6 +44,69 @@ namespace ProspectManager.Controllers
 
             db.SaveChanges();
 
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // GET: Contacts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contacts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Email,Birthdate")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contacts.Add(contact);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contact);
+        }
+
+        // GET: Contacts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // POST: Contacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            db.Contacts.Remove(contact);
+            db.SaveChanges();
             return RedirectToAction("Index");
         }
 
--- ProspectManager/Models/Contacts/Contact.cs
+++ ProspectManager/Models/Contacts/Contact.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Text;
+using System.Web;
 
 namespace ProspectManager.Models.Contacts
 {
