300a6561c75bb7a5e7948d6195836cda143f0a7a
Rob C <robcabardo@gmail.com>
2015-06-08 20:24:41 -0500

    Add homepage features

--- ProspectManager/Controllers/HomeController.cs
+++ ProspectManager/Controllers/HomeController.cs
@@ -4,15 +4,44 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using ProspectManager.Models;
 using ProspectManager.Models.Home;
 
 namespace ProspectManager.Controllers
 {
     public class HomeController : Controller
     {
+        DataContext db = new DataContext();
+
         public ActionResult Index()
         {
-            return View();
+            var threeDaysFromNow = DateTime.Now.AddDays(3);
+
+            var model = new IndexViewModel {
+                UpcomingTodos = db.TodoItems
+                    .Where(x => !x.Completed)
+                    .OrderBy(x => x.DueDate)
+                    .Take(10)
+                    .ToList(),
+                UpcomingMeetings = db.Meetings
+                    .Where(x => x.MeetingDate >= DateTime.Now && 
+                        x.MeetingDate < threeDaysFromNow)
+                    .OrderBy(x => x.MeetingDate)
+                    .Take(1000)
+                    .ToList()
+            };
+
+            return View(model);
+        }
+
+        protected override void  Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
--- ProspectManager/Models/Home/IndexViewModel.cs
+++ ProspectManager/Models/Home/IndexViewModel.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ProspectManager.Models.Meetings;
+using ProspectManager.Models.TodoItems;
 
 namespace ProspectManager.Models.Home
 {
     public class IndexViewModel
     {
-        public int Id { get; set; }
+        public List<TodoItem> UpcomingTodos { get; set; }
+
+        public List<Meeting> UpcomingMeetings { get; set; }
     }
 }
